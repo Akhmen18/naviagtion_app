babb999673e0b0885e83e1eff9b5bab0be5b63a2
Akhil Menon <akh180599@gmail.com>
2021-03-22 13:33:43 +0530

    HomePage done

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -8,6 +8,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage App'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Akhil'),
+              accountEmail: Text('Test@123.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.green.shade200,
+                child: Text('AM'),
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              trailing: Icon(Icons.home),
+            ),
+            ListTile(
+              title: Text('Category'),
+              trailing: Icon(Icons.category_rounded),
+              onTap: () => Navigator.of(context).pushNamed("/a"), //Route
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
